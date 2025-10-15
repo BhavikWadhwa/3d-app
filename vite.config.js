3458122a1e20bd78140b8a26c491f6f52f695da0
Bhavik <125359832+BhavikWadhwa@users.noreply.github.com>
2025-10-15 07:50:02 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    minify: false, // Disable minification to avoid eval bug
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+  },
 })
+
